351de031050ff92c3fc5a32776d3f394ed2a63b3
Oleg S <olegsolovyanenko@gmail.com>
2022-04-05 20:58:22 +0300

    Basic DAL almost ready

---
+++ Spotidie.DAL/EF/SpotidieContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Spotidie.DAL.Entities;
+
+
+namespace Spotidie.DAL.EF
+{
+    public class SpotidieContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        public SpotidieContext(DbContextOptions<SpotidieContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        //<-----Заполнение таблицы закомменчено----->
+        //static SpotidieContext()
+        //{
+        //    Database.SetInitializer<SpotidieContext>(new StoreDbInitializer());
+        //}
+    }
+
+    //public class StoreDbInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SpotidieContext>
+    //{
+    //    protected override void Seed(SpotidieContext db)
+    //    {
+    //        db.Users.Add(new User { UserName = "Admin", UserIsAdmin = true });
+    //        db.SaveChanges();
+    //    }
+    //}
+}
