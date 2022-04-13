234216de7f650985fa8d265f5d2a8dfb0cfc8af4
TIM <NARBAEV.T@YA.RU>
2022-04-13 15:39:38 +0300

    Added auth views

---
+++ Spotidie.WEB/Controllers/Authentication/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spotidie.Controllers.Authentication;
+
+public class AuthenticationController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
---
+++ Spotidie.WEB/Views/Authentication/Login.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spotidie.Views.Authentication;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
