3f83d9e89c61af7d3b9db1c5437d7244fc104408
Oleg S <olegsolovyanenko@gmail.com>
2022-04-22 21:39:34 +0300

    SomeFixes

--- Spotidie.WEB/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Spotidie.WEB/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -14,7 +14,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Spotidie.Areas.Identity.Data;
+using Spotidie.DAL.EF;
 
 namespace Spotidie.Areas.Identity.Pages.Account
 {
