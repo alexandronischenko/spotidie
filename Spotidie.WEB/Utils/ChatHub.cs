8d8839cfa4df06c38901a4e5a3fb36da0ff7f290
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2022-05-27 22:06:55 +0300

    Finished chat

--- Spotidie.WEB/Utils/ChatHub.cs
+++ Spotidie.WEB/Utils/ChatHub.cs
@@ -4,8 +4,8 @@ using System.Threading.Tasks;
 
 public class ChatHub : Hub
 {
-    public async Task Send(string message, string userName)
+    public async Task SendMessage(string message, string userName)
     {
-        await this.Clients.All.SendAsync("Send", message, userName);
+        await Clients.All.SendAsync("ReceiveMessage", message, userName);
     }
 }
--- Spotidie.WEB/wwwroot/js/chat.js
+++ Spotidie.WEB/wwwroot/js/chat.js
@@ -1,29 +1,41 @@
 "use strict";
 
-let connection = new signalR.HubConnectionBuilder().withUrl("/chat/hub").build();
+let connection = new signalR.HubConnectionBuilder().withUrl("/chat/hub").configureLogging(signalR.LogLevel.Information).build();
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
+    console.log("get")
+    console.log(message);
+    console.log(user);
+    
+    var h = document.createElement("h1");
+    h.style.color = "white"
+    document.getElementById("messagesList").appendChild(h);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+    h.textContent = `${user} says ${message}`;
 });
 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
+    console.log("connected")
 }).catch(function (err) {
+    console.log(err.toString())
     return console.error(err.toString());
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+
+    console.log("send")
+    console.log(message);
+    console.log(user);
     connection.invoke("SendMessage", user, message).catch(function (err) {
+        console.log(err.toString())
         return console.error(err.toString());
     });
     event.preventDefault();
